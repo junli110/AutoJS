17598836da4a3e46ea26c7ea8a45866ce15cb56c
junli110 <junli110@yeah.net>
2016-10-25 15:32:22 +0800

    uglify-js

--- walk.js
+++ walk.js
@@ -25,19 +25,47 @@ function walk(path, handleFile) {
         }  
     });  
 }  
+function getWebRoot(htmlFile) {
+    var dir = htmlFile
 
+    while (dir.length > 0) {
+        dir = dir.split(/[\\/]/)
+        dir.pop()
+        dir = dir.join("\\")
+        var files = fs.readdirSync(dir);
+        var result = ""
+        files.forEach(function (item) {
+            var tmpPath = dir + '\\' + item;
+            if (fs.statSync(tmpPath).isDirectory() && item == "WEB-INF") {
+                result = dir;
+            }
+        })
+        if (result) {
+            return result;
+        }
+    }
+}
 //js
-
-var jsp = require("uglify-js").parser;
-var pro = require("uglify-js").uglify; 
  var UglifyJS=require("uglify-js")
+var jsp = require("uglify-js").parser;
+var pro = require("uglify-js").uglify;
 function jsMinifier(flieIn, fileOut) {
-     var flieIn=Array.isArray(flieIn)? flieIn : [flieIn];
+     /*var flieIn=Array.isArray(flieIn)? flieIn : [flieIn];
      var origCode,ast,finalCode='';
      var result = UglifyJS.minify(flieIn, {
     //outSourceMap: "out.js.map"
 	});
-    fs.writeFileSync(fileOut, result.code, 'utf8');
+    fs.writeFileSync(fileOut, result.code, 'utf8');*/
+    var flieIn=Array.isArray(flieIn)? flieIn : [flieIn];
+    var origCode,ast,finalCode='';
+    for(var i=0; i<flieIn.length; i++) {
+        origCode = fs.readFileSync(flieIn[i], 'utf8');
+        ast = jsp.parse(origCode);
+        ast = pro.ast_mangle(ast);
+        ast= pro.ast_squeeze(ast);
+        finalCode +=';'+ pro.gen_code(ast);
+    }
+    fs.writeFileSync(fileOut, finalCode, 'utf8');
 }
  
 //jsMinifier('./file-src/test2.js', './file-smin/test-min.js');  //单个文件压缩
@@ -45,3 +73,4 @@ function jsMinifier(flieIn, fileOut) {
   
 exports.walk = walk; 
 exports.jsMinifier=jsMinifier;
+exports.getWebRoot=getWebRoot;
