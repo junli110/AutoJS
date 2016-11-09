d599f8dfb9f449d6c27329c3f91bace197060707
junli110 <junli110@yeah.net>
2016-11-09 14:46:55 +0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
-reg add "HKEY_CLASSES_ROOT\*\shell\CompressJSDebug\command" /d "%cd%\CompressJSDebug.bat %%1"   /f
-reg add "HKEY_CLASSES_ROOT\*\shell\CompressJSRelease\command"   /d "%cd%\CompressJSRelease.bat  %%1"   /f
+reg add "HKEY_CLASSES_ROOT\*\shell\CompressJSDebug\command" /d "%~dp0CompressJSDebug.bat %%1"   /f
+reg add "HKEY_CLASSES_ROOT\*\shell\CompressJSRelease\command"   /d "%~dp0CompressJSRelease.bat  %%1"   /f
 
 echo echo %%1%%> CompressJSDebug.bat
 echo node %cd%/AutoJS.js %%1%% debug>> CompressJSDebug.bat
